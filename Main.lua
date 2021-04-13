a1f2d4de562ba23e3063fc83574aced50a3a8251
KryptonMiner <79050258+KryptonMiner@users.noreply.github.com>
2021-04-13 16:47:14 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,6 @@
 if game.PlaceId == 6090639744 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KryptonMiner/BitcoinMiner/main/BaconSimulator.lua'))()
 end
+if game.PlaceId == 2232225661 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KryptonMiner/BitcoinMiner/main/KatanaSim.lua'))()
+end
